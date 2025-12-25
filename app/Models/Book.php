ee1cc6e983ea430c6473635dd2d8cfcf94d0fc1d
Bruno <brunorohling340@gmail.com>
2025-12-25 19:17:14 -0300

    2.8 class. Feat: local scope methods, popular and highest rated for Book class

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -24,4 +24,16 @@ class Book extends Model
         // Ever time that use a method to specify a "where" search, use the "scope" prefix
         return $query->where('title', 'LIKE', '%' . $title . '%');
     }
+
+    public function scopePopular(Builder $query): Builder
+    {
+        return $query->withCount('reviews')
+        ->orderBy('reviews_count', 'desc');
+    }
+
+    public function scopeHighestRated(Builder $query): Builder
+    {
+        return $query->withAvg('reviews', 'rating')
+        ->orderBy('reviews_avg_rating', 'desc');
+    }
 }
